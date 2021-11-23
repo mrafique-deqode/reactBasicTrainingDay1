877729d28f05722fd54d7688e8a5122fdf71ced6
Mohammed Rafique <mrafique@deqode.com>
2021-11-23 12:08:07 +0530

    Second commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,20 +2,74 @@
 
 console.log('App.js is running');
 
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+
+    var option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        renderJSX();
+    }
+};
+
+var resetData = function resetData() {
+    app.options = [];
+    renderJSX();
+};
+
+var app = {
+    title: "Hello world!",
+    options: []
+};
+
 var renderJSX = function renderJSX() {
-  var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h1',
-      null,
-      'Hello world'
-    )
-  );
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length > 0 ? 'Here are your options' : 'No options'
+        ),
+        React.createElement(
+            'ol',
+            null,
+            app.options.map(function (option) {
+                return React.createElement(
+                    'li',
+                    { key: option },
+                    option
+                );
+            })
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option', placeholder: 'Enter Data' }),
+            React.createElement(
+                'button',
+                null,
+                'Submit'
+            )
+        ),
+        React.createElement('br', null),
+        React.createElement(
+            'button',
+            { onClick: resetData },
+            'Reset'
+        )
+    );
 
-  var appRoot = document.getElementById('app');
+    var appRoot = document.getElementById('app');
 
-  ReactDOM.render(template, appRoot);
+    ReactDOM.render(template, appRoot);
 };
 
 renderJSX();
--- src/app.js
+++ src/app.js
@@ -1,9 +1,46 @@
 console.log('App.js is running')
 
+const onFormSubmit = (e) => {
+    e.preventDefault()
+
+    const option = e.target.elements.option.value
+
+    if (option) {
+        app.options.push(option)
+        e.target.elements.option.value = ''
+        renderJSX()
+    }
+}
+
+const resetData = () => {
+    app.options = []
+    renderJSX()
+}
+
+const app = {
+    title: "Hello world!",
+    options: []
+}
+
+
 const renderJSX = () => {
     const template = (
       <div>
-        <h1>Hello world</h1>
+        <h1>{app.title}</h1>
+        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+        <ol>
+            {
+                app.options.map((option) => {
+                    return <li key={option}>{option}</li>
+                })
+            }
+        </ol>
+        <form onSubmit={onFormSubmit}>
+            <input type="text" name="option" placeholder="Enter Data"/>
+            <button>Submit</button>
+        </form>
+        <br />
+        <button onClick={resetData}>Reset</button>
       </div>
     );
     
